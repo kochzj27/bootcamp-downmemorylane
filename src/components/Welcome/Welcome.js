6c99a900ea71f9d742b65bad5bd345fe390e31c2
Zach Koch <zachkoch@northwesternmutual.com>
2019-03-26 14:08:58 -0500

    added my accidentally deleted component

---
+++ src/components/Welcome/Welcome.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import './Welcome.css'
+
+const Welcome = (props) => {
+  return (
+    <div className='welcome'>
+      <h2>Welcome</h2>
+      <p>Choose an project to use!</p>
+      <br />
+    </div>
+  )
+}
+
+export default Welcome;
